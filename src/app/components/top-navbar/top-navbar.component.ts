125bb51b288276e92fbc06e10e9929c76638d984
marionorberto <marionorberto2018@gmail.com>
2024-08-29 17:06:13 +0100

    card-profile added and costumized

--- src/app/components/top-navbar/top-navbar.component.ts
+++ src/app/components/top-navbar/top-navbar.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-top-navbar',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink,
+    RouterLinkActive,
+  ],
   templateUrl: './top-navbar.component.html',
   styleUrl: './top-navbar.component.css'
 })
