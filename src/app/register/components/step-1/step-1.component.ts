ef540b67b1368205f1b7577023bfbca673dd4b4c
marionorberto <marionorberto2018@gmail.com>
2024-08-31 13:45:02 +0100

    style: add confetti effect to the register page
    added a confetti effect to the register page so that once the registration is over the effect is shown

--- src/app/register/components/step-1/step-1.component.ts
+++ src/app/register/components/step-1/step-1.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-step-1',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './step-1.component.html',
   styleUrl: './step-1.component.css'
 })
